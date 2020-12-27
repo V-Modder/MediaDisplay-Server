8a7c091a6a0a6a84cee857ddb1264de1bd25b198
V-Modder <V-Modder@web.de>
2020-12-28 00:47:59 +0100

    reset media

--- pystream/pystream.py
+++ pystream/pystream.py
@@ -257,5 +257,5 @@ class PyStream(QMainWindow):
 
     def restore_gui(self):
         self.stack.setCurrentIndex(2)
-        self.label_room_temp.setText("00:00")
-        self.label_time.setText("--°C")
+        self.label_room_temp.setText("--°C")
+        self.label_time.setText("00:00")
