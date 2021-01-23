46445f7b72eedd84468c89e89a7838dd5d7788b5
V-Modder <V-Modder@web.de>
2021-01-23 17:33:54 +0100

    fix gitudpate

--- pystream/gitupdater.py
+++ pystream/gitupdater.py
@@ -1,9 +1,12 @@
 import git 
 import subprocess
+import sys
 
 class GitUpdater:
     def update(path):
         g = git.cmd.Git(path)
         result = g.pull()
         if len(result) > 0:
-            subprocess.run(args=[(path / "media_display_server.py")])
+            #subprocess.run(args=["python", path + "/media_display_server.py"])
+            subprocess.Popen(args=["python", path + "/media_display_server.py"], cwd = path)
+            sys.exit()
