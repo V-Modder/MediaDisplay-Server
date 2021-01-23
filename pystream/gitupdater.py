c9246d050b090a7ddf778aa31f1d2725fcbb6ae9
V-Modder <V-Modder@web.de>
2021-01-23 22:16:18 +0100

    test

--- pystream/gitupdater.py
+++ pystream/gitupdater.py
@@ -7,6 +7,5 @@ class GitUpdater:
         g = git.cmd.Git(path)
         result = g.pull()
         if len(result) > 0:
-            #subprocess.run(args=["python", path + "/media_display_server.py"])
             subprocess.Popen(args=["python", path + "/media_display_server.py"], cwd = path)
             sys.exit()
